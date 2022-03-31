47af1ecb73fe52d00924da51f6d5e220f55b1080
Torsten Hellwig <torsten.hellwig93@gmail.com>
2022-03-31 16:22:43 +0200

    Reorganize user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -42,89 +42,56 @@ class UserInterface:
             command = input()
 
             if command.startswith('abort'):
-                command_parts = command.split()
-                if len(command_parts) != 2:
-                    print(COMMANDS['abort'])
-                    continue
-
-                self.api.abort_game(command_parts[1])
-
+                self._abort(command)
             elif command.startswith('challenge'):
-                command_parts = command.split()
-                command_length = len(command_parts)
-                if command_length < 2 or command_length > 6:
-                    print(COMMANDS['challenge'])
-                    continue
-
-                opponent_username = command_parts[1]
-                initial_time = int(command_parts[2]) if command_length > 2 else 60
-                increment = int(command_parts[3]) if command_length > 3 else 1
-                color = Challenge_Color(command_parts[4].lower()) if command_length > 4 else Challenge_Color.RANDOM
-                rated = command_parts[5].lower() == 'true' if command_length > 5 else True
-
-                self.api.create_challenge(opponent_username, initial_time, increment,
-                                          rated, color, Variant.STANDARD, 20)
-
+                self._challenge(command)
             elif command.startswith('matchmaking'):
-                command_parts = command.split()
-                if len(command_parts) > 2:
-                    print(COMMANDS['matchmaking'])
-                    continue
-
-                variant = Variant(command_parts[1]) if len(command_parts) == 2 else Variant.STANDARD
-
-                self._start_matchmaking(variant)
-
+                self._matchmaking(command)
             elif command == 'quit':
-                self.is_running = False
-                self.challenge_handler.stop()
-                print('Terminating programm ...')
-                if self.matchmaking:
-                    self.matchmaking.stop()
-                    self.matchmaking.join()
-                self.challenge_handler.join()
-
+                self._quit()
             elif command == 'reset':
-                if self.matchmaking:
-                    print('Can\'t reset matchmaking while running ...')
-                    continue
-
-                Matchmaking.reset_matchmaking()
-
+                self._reset()
             elif command == 'stop':
-                if not self.matchmaking:
-                    print('Matchmaking isn\'t currently running ...')
-                    continue
-
-                self.matchmaking.stop()
-                print('Stopping matchmaking ...')
-                self.matchmaking.join()
-                self.matchmaking = None
-                self.game_count.decrement()
-                self.challenge_handler.start_accepting_challenges()
-                print('Matchmaking has been stopped. And challenges are resuming ...')
-
+                self._stop()
             elif command == 'upgrade':
-                print('This will upgrade your account to a bot account.')
-                print('WARNING: This is irreversible. The account will only be able to play as a Bot.')
-                approval = input('Do you want to continue? [y/N]: ')
-
-                if approval.lower() not in ['y', 'yes']:
-                    print('Upgrade aborted.')
-                    continue
-
-                outcome = 'successful' if self.api.upgrade_account() else 'failed'
-                print(f'Upgrade {outcome}.')
-
+                self._upgrade()
             else:
-                print('These commands are supported by BotLi:\n')
-                for key, value in COMMANDS.items():
-                    print(f'{key:11}\t\t# {value}')
+                self._help()
 
-    def _start_matchmaking(self, variant: Variant) -> None:
+    def _abort(self, command: str) -> None:
+        command_parts = command.split()
+        if len(command_parts) != 2:
+            print(COMMANDS['abort'])
+            return
+
+        self.api.abort_game(command_parts[1])
+
+    def _challenge(self, command: str) -> None:
+        command_parts = command.split()
+        command_length = len(command_parts)
+        if command_length < 2 or command_length > 6:
+            print(COMMANDS['challenge'])
+            return
+
+        opponent_username = command_parts[1]
+        initial_time = int(command_parts[2]) if command_length > 2 else 60
+        increment = int(command_parts[3]) if command_length > 3 else 1
+        color = Challenge_Color(command_parts[4].lower()) if command_length > 4 else Challenge_Color.RANDOM
+        rated = command_parts[5].lower() == 'true' if command_length > 5 else True
+
+        self.api.create_challenge(opponent_username, initial_time, increment, rated, color, Variant.STANDARD, 20)
+
+    def _matchmaking(self, command: str) -> None:
         if self.matchmaking:
             print('matchmaking already running ...')
             return
+
+        command_parts = command.split()
+        if len(command_parts) > 2:
+            print(COMMANDS['matchmaking'])
+            return
+
+        variant = Variant(command_parts[1]) if len(command_parts) == 2 else Variant.STANDARD
 
         self.challenge_handler.stop_accepting_challenges()
 
@@ -135,6 +102,52 @@ class UserInterface:
 
         self.matchmaking = Matchmaking(self.config, self.api, variant)
         self.matchmaking.start()
+
+    def _quit(self) -> None:
+        self.is_running = False
+        self.challenge_handler.stop()
+        print('Terminating programm ...')
+        if self.matchmaking:
+            self.matchmaking.stop()
+            self.matchmaking.join()
+        self.challenge_handler.join()
+
+    def _reset(self) -> None:
+        if self.matchmaking:
+            print('Can\'t reset matchmaking while running ...')
+            return
+
+        Matchmaking.reset_matchmaking()
+
+    def _stop(self) -> None:
+        if not self.matchmaking:
+            print('Matchmaking isn\'t currently running ...')
+            return
+
+        self.matchmaking.stop()
+        print('Stopping matchmaking ...')
+        self.matchmaking.join()
+        self.matchmaking = None
+        self.game_count.decrement()
+        self.challenge_handler.start_accepting_challenges()
+        print('Matchmaking has been stopped. And challenges are resuming ...')
+
+    def _upgrade(self) -> None:
+        print('This will upgrade your account to a bot account.')
+        print('WARNING: This is irreversible. The account will only be able to play as a Bot.')
+        approval = input('Do you want to continue? [y/N]: ')
+
+        if approval.lower() not in ['y', 'yes']:
+            print('Upgrade aborted.')
+            return
+
+        outcome = 'successful' if self.api.upgrade_account() else 'failed'
+        print(f'Upgrade {outcome}.')
+
+    def _help(self) -> None:
+        print('These commands are supported by BotLi:\n')
+        for key, value in COMMANDS.items():
+            print(f'{key:11}\t\t# {value}')
 
 
 class Autocompleter:
