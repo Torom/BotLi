5db109a7e59cc3b7e0e00237c097b5f05e7ef57c
Torsten Hellwig <torsten.hellwig93@gmail.com>
2022-10-17 21:00:14 +0200

    Make some classes into dataclasses

--- challenge_response.py
+++ challenge_response.py
@@ -1,13 +1,10 @@
+from dataclasses import dataclass
+
 from aliases import Challenge_ID
 
 
+@dataclass
 class Challenge_Response:
-    def __init__(
-        self,
-        challenge_id: Challenge_ID | None = None,
-        success: bool = False,
-        has_reached_rate_limit: bool = False
-    ) -> None:
-        self.challenge_id = challenge_id
-        self.success = success
-        self.has_reached_rate_limit = has_reached_rate_limit
+    challenge_id: Challenge_ID | None = None
+    success: bool = False
+    has_reached_rate_limit: bool = False
