d2211153fd8b0680ab0df628f7f5e9e57832e2c1
Torsten Hellwig <torsten.hellwig93@gmail.com>
2022-10-18 23:00:57 +0200

    Don't use syzygy for variants engine

--- lichess_game.py
+++ lichess_game.py
@@ -514,14 +514,14 @@ class Lichess_Game:
             engine = chess.engine.SimpleEngine.popen_uci(self.config['engine']['path'])
             options = self.config['engine']['uci_options']
 
-        def not_managed(key: str): return not chess.engine.Option(key, '', None, None, None, None).is_managed()
-        options = {key: value for key, value in options.items() if not_managed(key)}
+            if self.config['engine']['syzygy']['enabled']:
+                delimiter = ';' if os.name == 'nt' else ':'
+                syzygy_path = delimiter.join(self.config['engine']['syzygy']['paths'])
+                options['SyzygyPath'] = syzygy_path
+                options['SyzygyProbeDepth'] = self.config['engine']['syzygy']['max_pieces']
 
-        if self.config['engine']['syzygy']['enabled']:
-            delimiter = ';' if os.name == 'nt' else ':'
-            syzygy_path = delimiter.join(self.config['engine']['syzygy']['paths'])
-            options['SyzygyPath'] = syzygy_path
-            options['SyzygyProbeDepth'] = self.config['engine']['syzygy']['max_pieces']
+        def is_managed(key: str): return chess.engine.Option(key, '', None, None, None, None).is_managed()
+        options = {key: value for key, value in options.items() if not is_managed(key)}
 
         engine.configure(options)
 
