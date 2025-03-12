24f61b2c7e3fb511e50d7b5f68861185ea4fcc75
vexalous <vexalous@hotmail.com>
2025-03-12 15:27:33 -0400

    Minimize try-except usage.

--- opponents.py
+++ opponents.py
@@ -164,17 +164,17 @@ class Opponents:
         return opponent
 
     def _load(self, matchmaking_file: str) -> list[Opponent]:
-        try:
             if not os.path.isfile(matchmaking_file):
                 return []
             
             with open(matchmaking_file, encoding='utf-8') as file:
-                data = json.load(file)
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError as e:
+                    print (f"Error while processing the file '{matchmaking_file}': {e}.")
+                    return []
             
             return [Opponent.from_dict(item) for item in data]
-        except Exception as e:
-            print(f"Error while processing the file '{matchmaking_file}': {e}.")
-            return []
 
     def _save(self, matchmaking_file: str) -> None:
         try:
