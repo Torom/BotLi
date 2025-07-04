744bf1b1000f7028407d614a4cb8a6dbeedd81ce
Torsten Hellwig <torsten.hellwig93@gmail.com>
2025-07-04 17:08:29 +0200

    Add short timeout to receive ongoing games first

--- user_interface.py
+++ user_interface.py
@@ -62,8 +62,12 @@ class User_Interface:
 
             signal.signal(signal.SIGTERM, self.signal_handler)
 
-            for command in commands:
-                await self._handle_command(command.split())
+            if commands:
+                # Short timeout to receive ongoing games first
+                await asyncio.sleep(0.5)
+
+                for command in commands:
+                    await self._handle_command(command.split())
 
             if not sys.stdin.isatty():
                 await self.game_manager_task
