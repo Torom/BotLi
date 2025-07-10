4301ceb38e21f962b30811398c2120ed46f6aa2e
Torsten Hellwig <torsten.hellwig93@gmail.com>
2025-07-10 10:50:16 +0200

    Reduce time for first move against bots The original 15 seconds was too close to the Lichess limit of 16 seconds in bullet tournaments.

--- engine.py
+++ engine.py
@@ -82,7 +82,7 @@ class Engine:
                         increment: float
                         ) -> tuple[chess.Move, chess.engine.InfoDict]:
         if len(board.move_stack) < 2:
-            time_limit = 15.0 if self.opponent.is_engine else 5.0
+            time_limit = 10.0 if self.opponent.is_engine else 5.0
             if self.limit_config.time:
                 time_limit = min(time_limit, self.limit_config.time)
 
