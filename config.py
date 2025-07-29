3e93947d36d80dfb826e3054f4abfbba5ae6f4fa
zonalraid45 <96386317+zonalraid45@users.noreply.github.com>
2025-07-29 20:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -253,6 +253,8 @@ class Config:
         opening_explorer_sections = [
             ['enabled', bool, '"enabled" must be a bool.'],
             ['priority', int, '"priority" must be an integer.'],
+            ['use_player', bool, '"use_player" must be a bool.'],
+            ['player', int, '"player" must be an integer.'],
             ['only_without_book', bool, '"only_without_book" must be a bool.'],
             ['use_for_variants', bool, '"use_for_variants" must be a bool.'],
             ['min_time', int, '"min_time" must be an integer.'],
@@ -270,8 +272,17 @@ class Config:
             if not isinstance(opening_explorer_section[subsection[0]], subsection[1]):
                 raise TypeError(f'`online_moves` `opening_explorer` field {subsection[2]}')
 
+        if opening_explorer_section['use_player']:  
+            if 'player' not in opening_explorer_section:  
+                raise RuntimeError('Your config does not have required '  
+                               '`online_moves` `opening_explorer` field `player` when `use_player` is true.')  
+            if not isinstance(opening_explorer_section['player'], str):  
+                raise TypeError('`online_moves` `opening_explorer` field "player" must be a string.')
+          
         return Opening_Explorer_Config(opening_explorer_section['enabled'],
                                        opening_explorer_section['priority'],
+                                       opening_explorer_section['use_player'],  
+                                       opening_explorer_section.get('player'), 
                                        opening_explorer_section['only_without_book'],
                                        opening_explorer_section['use_for_variants'],
                                        opening_explorer_section['min_time'],
