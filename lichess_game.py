0aa737123f4b1dc0d09f6bcfb572176cb55126cc
Supratsa <supraontwitch708@gmail.com>
2025-08-10 15:31:38 +0530

    Update lichess_game.py

--- lichess_game.py
+++ lichess_game.py
@@ -924,9 +924,9 @@ class Lichess_Game:
                             self.cloud_success_count,
                             self.cloud_total_attempts
                         )
-            else:
-                priority = self.config.online_moves.lichess_cloud.priority
-            opening_sources[self._make_cloud_move] = priority
+                    else:
+                        priority = self.config.online_moves.lichess_cloud.priority
+                    opening_sources[self._make_cloud_move] = priority
 
         if self.config.online_moves.chessdb.enabled:
             if not self.config.online_moves.chessdb.only_without_book or not self.book_settings.readers:
@@ -937,9 +937,9 @@ class Lichess_Game:
                             self.chessdb_success_count,
                             self.chessdb_total_attempts
                         )
-            else:
-                priority = self.config.online_moves.chessdb.priority
-            opening_sources[self._make_chessdb_move] = priority
+                    else:
+                        priority = self.config.online_moves.chessdb.priority
+                    opening_sources[self._make_chessdb_move] = priority
 
         move_sources += [opening_source
                          for opening_source, _
