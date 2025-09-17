74500ddc28eda77cc64538ffc7389f2735c892ac
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-09-17 13:30:49 +0530

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -16,6 +16,7 @@ from event_handler import Event_Handler
 from game_manager import Game_Manager
 from logo import LOGO
 from utils import parse_time_control
+from console import cprint   # ✅ use our colored printer
 
 try:
     import readline
@@ -45,13 +46,13 @@ EnumT = TypeVar('EnumT', bound=StrEnum)
 class User_Interface:
     async def main(self, commands: list[str], config_path: str, allow_upgrade: bool) -> None:
         self.config = Config.from_yaml(config_path)
-        print(f'{LOGO} • {self.config.version}', end='', flush=True)
-
+        cprint(f'{LOGO} • {self.config.version}',)  # ✅
+        
         async with API(self.config) as self.api:
             account = await self.api.get_account()
             username: str = account['username']
 
-            print(f' • {username}\n')
+            cprint(f' • {username}\n')  # ✅
 
             self.api.append_user_agent(username)
             await self._handle_bot_status(account.get('title'), allow_upgrade)
@@ -66,9 +67,7 @@ class User_Interface:
             signal.signal(signal.SIGTERM, self.signal_handler)
 
             if commands:
-                # Short timeout to receive ongoing games first
                 await asyncio.sleep(0.5)
-
                 for command in commands:
                     await self._handle_command(command.split())
 
@@ -88,40 +87,40 @@ class User_Interface:
 
     async def _handle_bot_status(self, title: str | None, allow_upgrade: bool) -> None:
         if 'bot:play' not in await self.api.get_token_scopes(self.config.token):
-            print('Your token is missing the bot:play scope. This is mandatory to use BotLi.\n'
-                  'You can create such a token by following this link:\n'
-                  'https://lichess.org/account/oauth/token/create?scopes[]=bot:play&description=BotLi')
+            cprint('Your token is missing the bot:play scope. This is mandatory to use BotLi.\n'
+                   'You can create such a token by following this link:\n'
+                   'https://lichess.org/account/oauth/token/create?scopes[]=bot:play&description=BotLi')
             sys.exit(1)
 
         if title == 'BOT':
             return
 
-        print('\nBotLi can only be used by BOT accounts!\n')
+        cprint('\nBotLi can only be used by BOT accounts!\n')
 
         if not sys.stdin.isatty() and not allow_upgrade:
-            print('Start BotLi with the "--upgrade" flag if you are sure you want to upgrade this account.\n'
-                  'WARNING: This is irreversible. The account will only be able to play as a BOT.')
+            cprint('Start BotLi with the "--upgrade" flag if you are sure you want to upgrade this account.\n'
+                   'WARNING: This is irreversible. The account will only be able to play as a BOT.')
             sys.exit(1)
         elif sys.stdin.isatty():
-            print('This will upgrade your account to a BOT account.\n'
-                  'WARNING: This is irreversible. The account will only be able to play as a BOT.')
+            cprint('This will upgrade your account to a BOT account.\n'
+                   'WARNING: This is irreversible. The account will only be able to play as a BOT.')
             approval = input('Do you want to continue? [y/N]: ')
 
             if approval.lower() not in ['y', 'yes']:
-                print('Upgrade aborted.')
+                cprint('Upgrade aborted.')
                 sys.exit()
 
         if await self.api.upgrade_account():
-            print('Upgrade successful.')
+            cprint('Upgrade successful.')
         else:
-            print('Upgrade failed.')
+            cprint('Upgrade failed.')
             sys.exit(1)
 
     async def _test_engines(self) -> None:
         for engine_name, engine_config in self.config.engines.items():
-            print(f'Testing engine "{engine_name}" ... ', end='', flush=True)
+            cprint(f'Testing engine "{engine_name}" ... ',)  # ✅
             await Engine.test(engine_config)
-            print('OK')
+            cprint('OK')
 
     async def _handle_command(self, command: list[str]) -> None:
         match command[0]:
@@ -157,15 +156,15 @@ class User_Interface:
 
     def _blacklist(self, command: list[str]) -> None:
         if len(command) != 2:
-            print(COMMANDS['blacklist'])
+            cprint(COMMANDS['blacklist'])
             return
 
         self.config.blacklist.append(command[1].lower())
-        print(f'Added {command[1]} to the blacklist.')
+        cprint(f'Added {command[1]} to the blacklist.')
 
     def _challenge(self, command: list[str]) -> None:
         if len(command) < 2 or len(command) > 6:
-            print(COMMANDS['challenge'])
+            cprint(COMMANDS['challenge'])
             return
 
         try:
@@ -176,20 +175,20 @@ class User_Interface:
             rated = command[4].lower() in ['true', 'yes', 'rated'] if len(command) > 4 else True
             variant = self._find_enum(command[5], Variant) if len(command) > 5 else Variant.STANDARD
         except ValueError as e:
-            print(e)
+            cprint(str(e))
             return
 
         challenge_request = Challenge_Request(opponent_username, initial_time, increment, rated, color, variant, 300)
         self.game_manager.request_challenge(challenge_request)
-        print(f'Challenge against {challenge_request.opponent_username} added to the queue.')
+        cprint(f'Challenge against {challenge_request.opponent_username} added to the queue.')
 
     def _clear(self) -> None:
         self.game_manager.challenge_requests.clear()
-        print('Challenge queue cleared.')
+        cprint('Challenge queue cleared.')
 
     def _create(self, command: list[str]) -> None:
         if len(command) < 3 or len(command) > 6:
-            print(COMMANDS['create'])
+            cprint(COMMANDS['create'])
             return
 
         try:
@@ -200,7 +199,7 @@ class User_Interface:
             rated = command[4].lower() in ['true', 'yes', 'rated'] if len(command) > 4 else True
             variant = self._find_enum(command[5], Variant) if len(command) > 5 else Variant.STANDARD
         except ValueError as e:
-            print(e)
+            cprint(str(e))
             return
 
         challenges: list[Challenge_Request] = []
@@ -211,42 +210,42 @@ class User_Interface:
                               increment, rated, Challenge_Color.BLACK, variant, 300))
 
         self.game_manager.request_challenge(*challenges)
-        print(f'Challenges for {count} game pairs against {opponent_username} added to the queue.')
+        cprint(f'Challenges for {count} game pairs against {opponent_username} added to the queue.')
 
     async def _join(self, command: list[str]) -> None:
         if len(command) < 2 or len(command) > 3:
-            print(COMMANDS['join'])
+            cprint(COMMANDS['join'])
             return
 
         password = command[2] if len(command) > 2 else None
         if await self.api.join_team(command[1], password):
-            print(f'Joined team "{command[1]}" successfully.')
+            cprint(f'Joined team "{command[1]}" successfully.')
 
     def _leave(self, command: list[str]) -> None:
         if len(command) != 2:
-            print(COMMANDS['leave'])
+            cprint(COMMANDS['leave'])
             return
 
         self.game_manager.request_tournament_leaving(command[1])
 
     def _matchmaking(self) -> None:
-        print('Starting matchmaking ...')
+        cprint('Starting matchmaking ...')
         self.game_manager.start_matchmaking()
 
     async def _quit(self) -> None:
         self.game_manager.stop()
-        print('Terminating program ...')
+        cprint('Terminating program ...')
         self.event_handler_task.cancel()
         await self.game_manager_task
 
     def _rechallenge(self) -> None:
         last_challenge_event = self.event_handler.last_challenge_event
         if last_challenge_event is None:
-            print('No last challenge available.')
+            cprint('No last challenge available.')
             return
 
         if last_challenge_event['speed'] == 'correspondence':
-            print('Correspondence is not supported by BotLi.')
+            cprint('Correspondence is not supported by BotLi.')
             return
 
         opponent_username: str = last_challenge_event['challenger']['name']
@@ -265,31 +264,31 @@ class User_Interface:
 
         challenge_request = Challenge_Request(opponent_username, initial_time, increment, rated, color, variant, 300)
         self.game_manager.request_challenge(challenge_request)
-        print(f'Challenge against {challenge_request.opponent_username} added to the queue.')
+        cprint(f'Challenge against {challenge_request.opponent_username} added to the queue.')
 
     def _reset(self, command: list[str]) -> None:
         if len(command) != 2:
-            print(COMMANDS['reset'])
+            cprint(COMMANDS['reset'])
             return
 
         try:
             perf_type = self._find_enum(command[1], Perf_Type)
         except ValueError as e:
-            print(e)
+            cprint(str(e))
             return
 
         self.game_manager.matchmaking.opponents.reset_release_time(perf_type)
-        print('Matchmaking has been reset.')
+        cprint('Matchmaking has been reset.')
 
     def _stop(self) -> None:
         if self.game_manager.stop_matchmaking():
-            print('Stopping matchmaking ...')
+            cprint('Stopping matchmaking ...')
         else:
-            print('Matchmaking isn\'t currently running ...')
+            cprint('Matchmaking isn\'t currently running ...')
 
     def _tournament(self, command: list[str]) -> None:
         if len(command) < 2 or len(command) > 4:
-            print(COMMANDS['tournament'])
+            cprint(COMMANDS['tournament'])
             return
 
         tournament_id = command[1]
@@ -300,16 +299,16 @@ class User_Interface:
 
     def _whitelist(self, command: list[str]) -> None:
         if len(command) != 2:
-            print(COMMANDS['whitelist'])
+            cprint(COMMANDS['whitelist'])
             return
 
         self.config.whitelist.append(command[1].lower())
-        print(f'Added {command[1]} to the whitelist.')
+        cprint(f'Added {command[1]} to the whitelist.')
 
     def _help(self) -> None:
-        print('These commands are supported by BotLi:\n')
+        cprint('These commands are supported by BotLi:\n')
         for key, value in COMMANDS.items():
-            print(f'{key:11}\t\t# {value}')
+            cprint(f'{key:11}\t\t# {value}')
 
     def _find_enum(self, name: str, enum_type: type[EnumT]) -> EnumT:
         for enum in enum_type:
